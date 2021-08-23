0ed943eed046bb46db3b8ffdb504ab77b41ddd3b
muji <muji@tmpfs.org>
2021-08-23 10:45:25 +0800

    Reduce lodash file.

--- src/lodash.js
+++ src/lodash.js
@@ -1,62 +1,14 @@
-/**
- * @license
- * Lodash <https://lodash.com/>
- * Copyright OpenJS Foundation and other contributors <https://openjsf.org/>
- * Released under MIT license <https://lodash.com/license>
- * Based on Underscore.js 1.8.3 <http://underscorejs.org/LICENSE>
- * Copyright Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
- */
 ;(function() {
 
+  function lodash(value) {
+    console.log('lodash')
+  }
 
-  /*--------------------------------------------------------------------------*/
+  function memoize () {
+    console.log('memoize')
+  }
 
-  /**
-   * Create a new pristine `lodash` function using the `context` object.
-   *
-   * @static
-   * @memberOf _
-   * @since 1.1.0
-   * @category Util
-   * @param {Object} [context=root] The context object.
-   * @returns {Function} Returns a new `lodash` function.
-   * @example
-   *
-   * _.mixin({ 'foo': _.constant('foo') });
-   *
-   * var lodash = _.runInContext();
-   * lodash.mixin({ 'bar': lodash.constant('bar') });
-   *
-   * _.isFunction(_.foo);
-   * // => true
-   * _.isFunction(_.bar);
-   * // => false
-   *
-   * lodash.isFunction(lodash.foo);
-   * // => false
-   * lodash.isFunction(lodash.bar);
-   * // => true
-   *
-   * // Create a suped-up `defer` in Node.js.
-   * var defer = _.runInContext({ 'setTimeout': setImmediate }).defer;
-   */
-  var runInContext = (function runInContext(context) {
-
-    function lodash(value) {
-      console.log('lodash')
-    }
-
-    function memoize () {
-      console.log('memoize')
-    }
-
-    lodash.memoize = memoize
-
-  });
-
-  var freeGlobal = typeof global == 'object' && global && global.Object === Object && global;
-  var freeSelf = typeof self == 'object' && self && self.Object === Object && self;
-  var root = freeGlobal || freeSelf || Function('return this')();
+  lodash.memoize = memoize
 
   //var freeExports = typeof exports == 'object' && exports && exports;
   //var freeModule = freeExports && typeof module == 'object' && module && module;
@@ -65,31 +17,10 @@
   var freeModule = freeExports && typeof module == 'object' && module && !module.nodeType && module;
 
   // Export lodash.
-  var _ = runInContext();
+  var _ = lodash;
 
-  // Some AMD build optimizers, like r.js, check for condition patterns like:
-  if (typeof define == 'function' && typeof define.amd == 'object' && define.amd) {
-    // Expose Lodash on the global object to prevent errors when Lodash is
-    // loaded by a script tag in the presence of an AMD loader.
-    // See http://requirejs.org/docs/errors.html#mismatch for more details.
-    // Use `_.noConflict` to remove Lodash from the global object.
-    root._ = _;
-
-    // Define as an anonymous module so, through path mapping, it can be
-    // referenced as the "underscore" module.
-    define(function() {
-      return _;
-    });
-  }
-  // Check for `exports` after `define` in case a build optimizer adds it.
-  else if (freeModule) {
-    // Export for Node.js.
-    (freeModule.exports = _)._ = _;
-    // Export for CommonJS support.
-    freeExports._ = _;
-  }
-  else {
-    // Export to the global object.
-    root._ = _;
-  }
+  // Export for Node.js.
+  (freeModule.exports = _)._ = _;
+  // Export for CommonJS support.
+  freeExports._ = _;
 }.call(this));
