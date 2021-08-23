348f209dc955c125ac0aeb56658d28570a23813c
muji <muji@tmpfs.org>
2021-08-23 10:54:20 +0800

    Reduce lodash.js.

--- src/lodash.js
+++ src/lodash.js
@@ -1,26 +1,14 @@
-;(function() {
+// This is a minimal reproduction from lodash@4.17.21
 
-  function lodash(value) {
-    console.log('lodash')
-  }
+function lodash(value) {
+  console.log('lodash')
+}
 
-  function memoize () {
-    console.log('memoize')
-  }
+function memoize () {
+  console.log('memoize')
+}
 
-  lodash.memoize = memoize
+lodash.memoize = memoize
 
-  //var freeExports = typeof exports == 'object' && exports && exports;
-  //var freeModule = freeExports && typeof module == 'object' && module && module;
-
-  var freeExports = typeof exports == 'object' && exports && !exports.nodeType && exports;
-  var freeModule = freeExports && typeof module == 'object' && module && !module.nodeType && module;
-
-  // Export lodash.
-  var _ = lodash;
-
-  // Export for Node.js.
-  (freeModule.exports = _)._ = _;
-  // Export for CommonJS support.
-  freeExports._ = _;
-}.call(this));
+//module.exports = lodash;
+exports = lodash;
